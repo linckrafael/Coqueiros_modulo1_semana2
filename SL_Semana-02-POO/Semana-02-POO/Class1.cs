83d3da1581a4015a82f1f6d6f628652bccf5849b
linckrafael <rafaelinck@hotmail.com>
2023-02-08 12:11:45 -0300

    Adicionar pasta semana2

---
+++ SL_Semana-02-POO/Semana-02-POO/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana_02_POO
+{
+    /// <summary>
+    /// Aqui criou uma classe
+    /// Foi criado um encapsulamento
+    /// </summary>
+    public class CentroDeCusto
+    {
+        public int Id { get; set; } //defaut 0
+        public string Descricao { get; set; } // null
+        public DateTime DataCriacao { get; set; }//01/01/1900 00:00
+        public decimal VerbaLiberada { get; set; }//0.0
+        public int AnoVerbaLiberada { get; set; }
+
+        //variavel privada
+        private decimal custoReal;
+
+        //construtor com parametros
+        public CentroDeCusto(int id, string descricao, DateTime dataCriacao, decimal verbaLiberada, int anoVerbaLiberada)
+        {
+            /*preencher as propriedades com os
+             * parametros recebidos do construtor
+             */
+            Id = id;
+            Descricao = descricao;
+            DataCriacao = dataCriacao;
+            VerbaLiberada = verbaLiberada;
+            AnoVerbaLiberada = anoVerbaLiberada;
+        }
+        //Novo metodo que vai preeencher a variavel privada chamada custo real com o valor do parametro recebido(custoRealNovo)
+        public void CalcularCustoReal(decimal custoRealNovo) {
+            custoReal = custoRealNovo;
+            Console.WriteLine($"O novo custo real do centro de custo {Descricao}, com o id {Id} é {custoReal} ");
+        }
+    }
+}
---
+++ SL_Semana-02-POO/Semana-02-POO/Program.cs
@@ -0,0 +1,5 @@
+﻿// Declarar uma variavel com instancia da minha classe
+using Semana_02_POO;
+
+CentroDeCusto nomeVariavel = new CentroDeCusto(1, "teste", DateTime.Now, 1500M, 2023);
+nomeVariavel.CalcularCustoReal(10);
